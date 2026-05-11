6d451580db0b2d01fa3bf904ebee848c275d149b
ndurregoch <ingdavidurrego@gmail.com>
2026-05-11 10:28:14 -0400

    Handle blank input errors in server

--- server.py
+++ server.py
@@ -9,6 +9,9 @@ def emotion_detector_route():
 
     response = emotion_detector(text_to_analyze)
 
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+
     anger = response['anger']
     disgust = response['disgust']
     fear = response['fear']
